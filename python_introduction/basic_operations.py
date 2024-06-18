8765303b105e51419753c294090ffab1aaf5546c
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-06-18 16:06:24 +0100

    Python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10 
+number2 = 5
+
+sum = number1 + number2
+subtract = number1 - number2
+product = number1 * number2
+
+print(f'sum of {number1} and {number2} is {sum}')
+print(f'subtraction of {number1} and {number2} is {subtract}')
+print(f'product of {number1} and {number2} is {product}')
