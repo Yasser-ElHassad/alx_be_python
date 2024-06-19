bebc6b1502df6cb27b73f28273ec1a82c3bfa271
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-06-19 12:52:10 +0100

    Finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,6 +7,6 @@ annual_interest = 0.05
 
 projected_saving = month_saving * 12 + (month_saving * 12 * annual_interest)
 
-print(f'Your monthly savings are ${month_saving}')
+print(f'Your monthly savings are ${month_saving}.')
 print(f'Projected savings after one year, with interest, is: ${int(projected_saving)}.')
 
