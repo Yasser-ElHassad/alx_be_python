a76213a80aaef57d20c3768f0da185f474815a6a
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-07 18:29:12 +0100

    Arithmetic Operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        print(num1 + num2)
+    elif operation == 'subtract':
+        print(num1 - num2)
+    elif operation == 'multiply':
+        print(num1 * num2)
+    elif operation == 'divide':
+        division = num1 / num2
+        if num2 == 0:
+            print('Zero division Error')
+        else:
+            print(division)
+
