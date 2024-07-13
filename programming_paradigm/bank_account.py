4478de27f575fd1dd90894523374a3377801695b
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-13 17:23:02 +0100

    account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,14 +7,14 @@ class BankAccount:
 
     def deposit(self,amount):
         self.account_balance += amount
-        return f'Deposited: ${self.account_balance}'
     
     def withdraw(self, amount):
         if amount < self.account_balance:
             self.account_balance -= amount
-            return f'Withdrew: ${amount}'
+            return True
         else:
-            return f'Insufficient funds.'
+            return False
+        
         
     def display_balance(self):  
          return f"Current Balance: ${self.account_balance}"
