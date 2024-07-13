95728953a64d32dbfa6b6aa21cd56cf9a0e9118e
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-13 16:37:19 +0100

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
         else:
             return f'Insufficient funds'
     def display_balance(self):
-        print(f"Current balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance}")
 
 
