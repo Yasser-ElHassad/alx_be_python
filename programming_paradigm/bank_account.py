3aed779784a62c0cc13990b119a2df0e154ff156
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-13 18:01:48 +0100

    account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,9 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = account_balance
+        if account_balance != 0:
+            self.account_balance = account_balance
+        else:
+             self.account_balance = 0
         
 
     def deposit(self,amount):
