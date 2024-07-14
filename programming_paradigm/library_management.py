3129e961cee8c9cdaaba57c3efda641424c1fdd0
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-14 20:25:23 +0100

    library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,30 @@
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        _is_checked_out = 0
+
+    def __str__(self):
+        if (self.title,self.author) in self.books:
+            return (self.title, self.author)
+        
+
+    
+class Library(Book):
+    def __init__(self):
+        self._books = []
+    def add_book(self):
+        self._books.append(self.title,self.author)
+
+    def check_out_book(self,title):
+        for i in self._books:
+            if title != i[0]:
+                print(f"{i[0]} by {i[1]}")
+
+    def return_book(selftitle):
+        pass 
+
+    def list_available_books(self):
+        for i in self._books:
+            print(f"{i[0]} by {i[1]}")
