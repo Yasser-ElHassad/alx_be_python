af1ae3d4a0bd16736af1ccea4b52b37e797e4abe
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-14 19:30:18 +0100

    unitest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,16 +9,16 @@ class TestSimpleCalcultor(unittest.TestCase):
         self.assertEqual(self.calc.add(2,3), 5)
         
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10,8), 2)
         self.assertEqual(self.calc.subtract(8,10), -2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(8,10), 80)
         self.assertEqual(self.calc.multiply(8,-10), -80)
         self.assertEqual(self.calc.multiply(-8,-10), 80)
 
-    def test_divide(self):
+    def test_division(self):
         try:
             self.assertEqual(self.calc.divide(8,10), 0.8)
             self.assertEqual(self.calc.divide(8, 0), None)
